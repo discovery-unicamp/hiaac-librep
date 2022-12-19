9d02fa730e5858954c3a060ff79709a86a21cb42
Otavio Napoli <otavio.napoli@gmail.com>
2022-12-19 15:37:07 -0300

    Added instructions to contribute

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # hiaac-librep
 
-More information at XXX
+
+## Installation
+
+To install `librep`, you may use:
+
+```
+pip install git+https://github.com/otavioon/hiaac-librep.git
+```
+
+### Pip optional dependencies
+
+- `deep-learning`: install packages to run deep learning models
+- `dev`: install development packages
+- `docs`: install packages to build documentation
+
+## Contributing
+
+In order to contribute with `librep` you may want to:
+
+1. Clone librep's repository:
+
+```
+git clone https://github.com/otavioon/hiaac-librep.git
+```
+
+2. Create a python virtual environment and activate it (requires Python >= 3.8):
+
+```
+cd hiaac-librep
+python -m venv .librep-venv
+source .librep-venv/bin/activate
+```
+
+3. Install librep development packages, in editable mode
+
+```
+pip install -e .[dev]
+```
+
+4. Run tests
+
+```
+pytest
+```
---
+++ tests/test_simple_imports.py
@@ -0,0 +1,27 @@
+def test_simple_imports():# Third-party imports
+    import numpy as np
+    import pandas as pd
+    import matplotlib.pyplot as plt
+    import umap
+    import plotly.express as px
+    import plotly.graph_objects as go
+
+    # Librep imports
+    from librep.utils.dataset import PandasDatasetsIO
+    from librep.datasets.har.loaders import (
+        KuHar_BalancedView20HzMotionSenseEquivalent,
+        MotionSense_BalancedView20HZ,
+        ExtraSensorySense_UnbalancedView20HZ,
+        CHARM_BalancedView20Hz,
+        WISDM_UnbalancedView20Hz,
+        UCIHAR_UnbalancedView20Hz
+    )
+    from librep.datasets.multimodal import PandasMultiModalDataset, TransformMultiModalDataset, WindowedTransform
+    from librep.transforms.fft import FFT
+    from librep.transforms. stats import StatsTransform
+    from librep.utils.workflow import SimpleTrainEvalWorkflow, MultiRunWorkflow
+    from librep.estimators import RandomForestClassifier, SVC, KNeighborsClassifier
+    from librep.metrics.report import ClassificationReport
+    from librep.transforms.resampler import SimpleResampler
+
+    pass
