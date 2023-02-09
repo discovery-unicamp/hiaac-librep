67ceb7991682bc5ab2129490e0a63984379ca676
Otavio Napoli <otavio.napoli@gmail.com>
2023-02-09 09:41:44 -0300

    Put librep.base.* to  to librep.base __init__
    Signed-off-by: Otavio Napoli <otavio.napoli@gmail.com>

---
+++ src/librep/base/__init__.py
@@ -0,0 +1,4 @@
+from .data import Dataset, IterableDataset, Subset, ChainDataset
+from .estimator import Estimator
+from .evaluators import SupervisedEvaluator, UnsupervisedEvaluator, CustomSimpleEvaluator, CustomMultiEvaluator, Evaluators
+from .transform import Transform
