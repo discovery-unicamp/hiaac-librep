cad97b1501a8fa9608a7b9f48718407e22006977
Darlinne Hubert Palo Soto <ra264955@lmcad-dl-4.ic.unicamp.br>
2023-08-07 08:52:45 -0300

    LSTM - added patience

--- src/librep/transforms/lstm.py
+++ src/librep/transforms/lstm.py
@@ -1,15 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-#########################################################################
-# Defining constants
-#########################################################################
-SEQUENCE_LENGTH = 60
-INPUT_SIZE = 6
-N_CLASSES = 7
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-N_LAYERS = 1 # stacked LSTMs
+from sklearn.model_selection import train_test_split
+import numpy as np
+from copy import deepcopy
 
 #########################################################################
 # Creating LSTM model
@@ -17,28 +11,30 @@ N_LAYERS = 1 # stacked LSTMs
 
 # the reducer
 class LSTM(nn.Module):
-  def __init__(self, hidden_size):
+  def __init__(self, latent_dim, device, n_layers, input_size):
     super(LSTM, self).__init__()
+    self.device = device
+    self.n_layers = n_layers
     self.lstm = nn.LSTM(
-        hidden_size = hidden_size,
-        input_size=INPUT_SIZE,
-        num_layers=N_LAYERS,
+        hidden_size = latent_dim,
+        input_size=input_size,
+        num_layers=n_layers,
         batch_first=True
         )
-    self.hidden_size=hidden_size
+    self.latent_dim=latent_dim
 
   def forward(self, x):
-    h_0 = torch.zeros(N_LAYERS, x.shape[0], self.hidden_size).double().to(DEVICE)
-    c_0 = torch.zeros(N_LAYERS, x.shape[0], self.hidden_size).double().to(DEVICE)
+    h_0 = torch.zeros(self.n_layers, x.shape[0], self.latent_dim).float().to(self.device)
+    c_0 = torch.zeros(self.n_layers, x.shape[0], self.latent_dim).float().to(self.device)
     h_n, _ = self.lstm(x, (h_0, c_0))
     return h_n[:,-1] # returning the last hidden state
 
 # a model to train the reducer
 class TrainingModule(nn.Module):
-  def __init__(self, lstm_reducer):
+  def __init__(self, lstm_reducer, n_classes=7):
     super(TrainingModule, self).__init__()
     self.lstm = lstm_reducer
-    self.fc = nn.Linear(self.lstm.hidden_size, N_CLASSES)
+    self.fc = nn.Linear(self.lstm.latent_dim, n_classes)
 
   def forward(self, x):
     x = self.lstm(x)
@@ -54,86 +50,85 @@ from torch.utils.data import TensorDataset, DataLoader
 from torch.optim import Adam
  
 class LSTMTrainer(Transform):
-  def __init__(self, weight_file=None, n_epochs=40, learning_rate=1e-4, batch_size=32, hidden_size=20):
+  def __init__(self, weight_file=None, n_epochs=40, learning_rate=1e-4,
+               batch_size=32, latent_dim=20, n_classes=7, n_layers=1,
+               sequence_length=60, input_size=6, patience=5):
     assert weight_file is not None, 'weight_file must be specified'
     
-    self.model = TrainingModule(LSTM(hidden_size))
+    self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    self.input_size = input_size
+    self.sequence_length = sequence_length
+    self.model = TrainingModule(LSTM(latent_dim=latent_dim, device=self.device, n_layers=n_layers, input_size=input_size), n_classes=n_classes)
     self.epochs = n_epochs
     self.lr = learning_rate
     self.batch_size = batch_size
     self.fit_dataset = weight_file
+    self.patience = patience
 
-    print('Using device:', DEVICE)
+    print('Using device:', self.device)
     
   # Reshape the dataset into LSTM input 
   def _reshapeToLSTM(self, tensor):
-    tensor = torch.reshape(tensor, (tensor.shape[0], INPUT_SIZE, SEQUENCE_LENGTH)) # (n_samples, 6, 60)
+    tensor = torch.reshape(tensor, (tensor.shape[0], self.input_size, self.sequence_length)) # (n_samples, 6, 60)
     return torch.permute(tensor, (0, 2, 1)) # (n_samples, seq_length, H_in)
   
-  def _training_loop(self, dataloaders, dataset_sizes):  
+  def __one_epoch(self, dataloader: DataLoader, mode='train'):
+    cumulative_loss = 0
+    loss_counter = 0
+    for inputs, labels in dataloader:
+      inputs = inputs.float().to(self.device)
+      labels = labels.type(torch.LongTensor).to(self.device)
+      # zero the parameter gradients
+      self.optimizer.zero_grad()
+      if mode == 'train':
+        self.model.train()
+        outputs = self.model(inputs.float())
+        loss = self.criterion(outputs, labels)
+        loss.backward()
+        self.optimizer.step()
+      else:
+        self.model.eval()
+        outputs = self.model(inputs.float())
+        loss = self.criterion(outputs, labels)
+      cumulative_loss += loss.item() * inputs.size(0)
+      loss_counter += 1
+    return cumulative_loss / loss_counter
+  
+  def _training_loop(self, train_dataloader, val_dataloader):  
     ############### Training setup ##############
     # loss function
-    criterion = nn.NLLLoss()
+    self.criterion = nn.NLLLoss()
 
     # Observe that all parameters are being optimized
-    optimizer = Adam(self.model.parameters(), lr=self.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
+    self.optimizer = Adam(self.model.parameters(), lr=self.lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)
 
     # sending the model to the GPU
-    model = self.model.double().to(DEVICE)
+    model = self.model.float().to(self.device)
 
     ############### Start training ##############
     epoch_loss_train = []
     epoch_loss_val = []
     epoch_acc_train = []
     epoch_acc_val = []
-
-    # only run validation if there is a validation set
-    if self.validation:
-      phases = ['train', 'validation']
-    else:
-      phases = ['train']  
+    loss_threshold = np.inf
+    patience_counter = 0
 
     for epoch in range(self.epochs):
-      for phase in phases:
-        running_loss = 0.0
-        running_corrects = 0
-
-        for inputs, labels in dataloaders[phase]:
-          inputs = inputs.double().to(DEVICE)
-          labels = labels.type(torch.LongTensor).to(DEVICE)
-
-          # zero the parameter gradients
-          optimizer.zero_grad()
-
-          # forward prop
-          with torch.set_grad_enabled(phase == 'train'):
-            outputs = model(inputs.double())
-            _, preds = torch.max(outputs, 1)
-            loss = criterion(outputs, labels)
-
-            # back prop
-            if phase == 'train':
-              loss.backward()
-              optimizer.step()
-
-          # statistics
-          running_loss += loss.item() * inputs.size(0)
-          running_corrects += torch.sum(preds == labels.data)
-
-        epoch_loss = running_loss / dataset_sizes['train']
-        epoch_acc = running_corrects.double() / dataset_sizes['train']
-
-        print('{} loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
-
-        if phase == 'train':
-          epoch_loss_train.append(epoch_loss)
-          epoch_acc_train.append(epoch_acc)
-        else:
-          epoch_loss_val.append(epoch_loss)
-          epoch_acc_val.append(epoch_acc)
-
-      print('End of epoch {}/{}'.format(epoch, self.epochs - 1))
+      patience_counter += 1
+      train_loss = self.__one_epoch(train_dataloader, mode='train')
+      print(f'EPOCH:{epoch} TRAIN loss: {train_loss:.4f}')
+      # if 'validation' in dataloaders:
+      validation_loss = self.__one_epoch(val_dataloader, mode='validation')
+      print(f'EPOCH:{epoch} VALIDATION loss: {validation_loss:.4f}')
+      epoch_loss_val.append(validation_loss)
+      epoch_loss_train.append(train_loss)
       print('-' * 10)
+      if validation_loss < loss_threshold:
+        patience_counter = 0
+        self.model_best_state_dict = deepcopy(self.model.state_dict())
+        loss_threshold = validation_loss
+      if self.patience and patience_counter > self.patience:
+        break
 
     self.losses = {
       'train': epoch_loss_train,
@@ -143,39 +138,35 @@ class LSTMTrainer(Transform):
       'train': epoch_acc_train,
       'val': epoch_acc_val
     }
-
+    self.model.load_state_dict(self.model_best_state_dict)
     return model
   
-  def fit(self, X, y, X_val = None, y_val = None):    
-    ############### Organizing data ##############    
-    x_train = self._reshapeToLSTM(torch.Tensor(X))    
-    y_train = torch.Tensor(y)
+  def fit(self, X, y, X_val = None, y_val = None):
+    ### If there is no X_val, create it from X
+    if X_val is None:
+      X, X_val, y, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    ### Organizing data    
+    x_train = self._reshapeToLSTM(torch.Tensor(X))
+    y_train = torch.Tensor(np.array(y))
+    x_val = self._reshapeToLSTM(torch.Tensor(X_val))
+    y_val = torch.Tensor(np.array(y_val))
     
-    inputs, labels = dict(), dict()  
-    if X_val is not None and y_val is not None:
-      x_val = self._reshapeToLSTM(torch.Tensor(x_val))
-      y_val = torch.Tensor(y_val) 
-      
-      inputs = { 'train': x_train, 'validation': x_val }
-      labels = { 'train': y_train, 'validation': y_val }
-      phases = ['train', 'validation']
-      self.validation = True
-      
-    else:
-      inputs = { 'train': x_train }
-      labels = { 'train': y_train }
-      phases = ['train']
-      self.validation = False
-    
-    # creating the dataloaders
-    datasets, dataloaders, dataset_sizes = dict(), dict(), dict()
-    for phase in phases:
-      datasets[phase] = TensorDataset(inputs[phase], labels[phase])
-      dataloaders[phase] = DataLoader(datasets[phase], batch_size=self.batch_size, shuffle=True, num_workers=0)
-      dataset_sizes[phase] = len(datasets[phase])      
+    train_dataloader = DataLoader(
+      TensorDataset(x_train, y_train),
+      batch_size=self.batch_size,
+      shuffle=True,
+      num_workers=0
+    )
+    val_dataloader = DataLoader(
+      TensorDataset(x_val, y_val),
+      batch_size=self.batch_size,
+      shuffle=True,
+      num_workers=0
+    )
     
     ############### Start training loop ##############
-    self.model = self._training_loop(dataloaders, dataset_sizes)
+    self.model = self._training_loop(train_dataloader, val_dataloader)
     self.reducer = self.model.lstm
     
     # saving weights
@@ -184,7 +175,7 @@ class LSTMTrainer(Transform):
     
   def transform(self, X):
     X = self._reshapeToLSTM(torch.Tensor(X))
-    X = X.double().to(DEVICE)
-    reducer = self.reducer.double().to(DEVICE)
+    X = X.float().to(self.device)
+    reducer = self.reducer.float().to(self.device)
     h_n = reducer(X)
     return h_n.cpu().detach().numpy()
