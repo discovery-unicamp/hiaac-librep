4cbf66a9e974afd1244d2ec59b849b38763f0451
Hubert Palo <darlinnep@gmail.com>
2024-03-16 13:45:47 -0300

    Update topo_ae_model.py

--- src/librep/transforms/topo_ae_model.py
+++ src/librep/transforms/topo_ae_model.py
@@ -58,6 +58,7 @@ class ConvTAEModule(nn.Module):
         # --------------------------------------------------------------
         # -------------------- BUILDING THE ENCODER --------------------
         # --------------------------------------------------------------
+        sizes_to_compare = []
         conv_channels_sequences = {
             0: [],
             1: [256],
@@ -95,6 +96,7 @@ class ConvTAEModule(nn.Module):
             # Testing the conv layer
             test_data = torch.randn(current_input_size)
             test_data = conv_layer_to_append(test_data)
+            sizes_to_compare.append(test_data.size())
             # Update values
             current_input_size = test_data.size()
             in_channels = test_data.size(0)
@@ -116,6 +118,7 @@ class ConvTAEModule(nn.Module):
                 temporal_decoder_layers.append(unpooling_to_append)
                 # Testing the pooling
                 test_data = pooling_to_append(test_data)
+                sizes_to_compare.append(test_data.size())
                 # Update values
                 current_input_size = test_data.size()
                 in_channels = test_data.size(0)
@@ -133,6 +136,7 @@ class ConvTAEModule(nn.Module):
         for index, dim in enumerate(dimensions[:-1]):
             layer = nn.Linear(dim, dimensions[index+1])
             test_data = layer(test_data)
+            sizes_to_compare.append(test_data.size())
             encoder_layers.append(layer)
             encoder_layers.append(nn.ReLU())
         # Delete the last ReLU()
@@ -140,6 +144,7 @@ class ConvTAEModule(nn.Module):
         print('LATENT', test_data.size(), 'DIMENSIONS', dimensions, '\n')
         # Building the encoder
         self.encoder = nn.Sequential(*encoder_layers)
+        print(sizes_to_compare, '\n')
         # --------------------------------------------------------------
         # -------------------- BUILDING THE DECODER --------------------
         # --------------------------------------------------------------
